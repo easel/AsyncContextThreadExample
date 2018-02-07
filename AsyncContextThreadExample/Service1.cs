4099b33028fb7c0fbc48f66e5c1681d643006ef1
Erik LaBianca <erik.labianca@carahsoft.com>
2018-02-07 11:51:20 -0500

    Initial commit

---
+++ AsyncContextThreadExample/Service1.cs
@@ -0,0 +1,53 @@
+﻿using Nito.AsyncEx;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using Nito.AsyncEx.Synchronous;
+
+
+namespace AsyncContextThreadExample
+{
+    public partial class Service1 : ServiceBase
+    {
+        private readonly AsyncContextThread thread = null;
+        private Boolean isRunning = false;
+        public Service1()
+        {
+            InitializeComponent();
+            thread = new AsyncContextThread();
+        }
+
+        private async Task AsyncMain()
+        {
+            EventLog.WriteEntry("AsyncMain Started");
+            while (isRunning)
+            {
+                await Task.Delay(500);
+                await Task.Run(() =>
+                {
+                    EventLog.WriteEntry("AsyncMain Re-Checking");
+                });
+            }
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            isRunning = true;
+            thread.Factory.Run(AsyncMain);
+            EventLog.WriteEntry("Starting AsyncContextThreadExampleService");
+        }
+
+        protected override void OnStop()
+        {
+            EventLog.WriteEntry("Stopping Service");
+            isRunning = false;
+            thread.JoinAsync().WaitAndUnwrapException();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Minimal example of a windows service built using .NET leveraging AsyncContextThread[1] instead of blocking threads for background work. 
+
+
+1. https://github.com/StephenCleary/AsyncEx
